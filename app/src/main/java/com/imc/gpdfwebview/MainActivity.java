29f2e01acc4dcf013424cff740c5d0ca9889442c
ec21366 <s.abushady@se21.qmul.ac.uk>
2025-01-14 12:05:14 +0100

    add splash page

---
+++ app/src/main/java/com/imc/gpdfwebview/MainActivity.java
@@ -0,0 +1,58 @@
+package com.imc.gpdfwebview;
+
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.druckerforum.org");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String s, String s1, String s2, String s3, long l) {
+                DownloadManager.Request myRequest = new DownloadManager.Request(Uri.parse(s));
+                myRequest.allowScanningByMediaScanner();
+                myRequest.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+                DownloadManager myManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                myManager.enqueue(myRequest);
+                Toast.makeText(MainActivity.this, "The PDF is Downloading...", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
